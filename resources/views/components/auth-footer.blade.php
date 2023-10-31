359f74641c827903ff01924f653487872e420800
HaekalSambas <haikalsambass@gmail.com>
2023-10-31 18:08:16 +0800

    login,register,forgot password

---
+++ resources/views/components/auth-footer.blade.php
@@ -0,0 +1,3 @@
+  <div class="simple-footer">
+      Copyright &copy; Simpadu-C030322127
+  </div>
---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright Simpadu-C030322127<div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad
+            Yusup Haekal Sambas</a>
+    </div>
+    <div class="footer-right">
+        2.3.0
+    </div>
+</footer>
